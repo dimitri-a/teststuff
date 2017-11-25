ac27a83d0cd8cd8603aac764e72a328e8f1f1cf9
dimitric <couwenberg@gmail.com>
2017-11-25 19:27:13 +1100

    chore(component): Updated Modal removed mobile style and moved to Employee

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -11,28 +11,53 @@ export default class Employee extends Component {
         }
     }
 
-    modalToggle = () =>{
-        this.setState({ modalOpened: !this.state.modalOpened })
+    modalToggle = () => {
+        this.setState({modalOpened: !this.state.modalOpened})
     }
 
     render() {
         return (
-            <div onClick={this.modalToggle}>
-                <div className='col-lg-4 employee'>
-                    <div className="col-lg-4 text-left">
-                        <img src={this.props.details.avatar} alt=""/>
-                    </div>
-                    <div className="col-lg-8">
-                        <div>
+            <div>
+                <div className='hidden-xs' onClick={this.modalToggle}>
+                    <div className='col-lg-4 employee'>
+                        <div className="col-lg-4 text-left">
+                            <img src={this.props.details.avatar} alt=""/>
+                        </div>
+                        <div className="col-lg-8">
+                            <div>
                             <span className='name'> {this.props.details.firstName} &nbsp;
-                            {this.props.details.lastName}</span>
+                                {this.props.details.lastName}</span>
+                                <div>
+                                    {this.props.details.bio.slice(0, 80)}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Modal details={this.props.details} modalOpened={this.state.modalOpened}
+                           toggle={this.modalToggle}></Modal>
+                </div>
+
+                <div className='visible-xs'>
+                    <div className='col-lg-4 employee'>
+                        <div className="col-lg-4 text-left">
+                            <img src={this.props.details.avatar} alt=""/>
+                        </div>
+                        <div className="col-lg-8">
+                            <div>
+                            <span className='name'> {this.props.details.firstName} &nbsp;
+                                {this.props.details.lastName}</span>
+
+                            </div>
+                            <div>{this.props.details.jobTitle}</div>
+                            <div>{this.props.details.age}</div>
+                            <div>{Date(this.props.details.dateJoined)}</div>
                             <div>
                                 {this.props.details.bio.slice(0, 80)}
                             </div>
                         </div>
                     </div>
+
                 </div>
-                <Modal details={this.props.details} modalOpened ={this.state.modalOpened} toggle={this.modalToggle}></Modal>
             </div>
         );
     }
