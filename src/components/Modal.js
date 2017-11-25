90b589a4c8374caa5629e98a6c202f6a93c1f395
dimitric <couwenberg@gmail.com>
2017-11-25 19:27:40 +1100

    chore(component): Updated Modal removed mobile style and moved to Employee

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,7 +9,8 @@ export default class Modal extends Component {
 
 
     toggle = () => {
-        this.props.toggle();
+        console.log('toggle');
+        //this.props.toggle();
     }
 
     render() {
@@ -20,28 +21,31 @@ export default class Modal extends Component {
         const containerClass = this.props.modalOpened ? 'modal-container modal-container-active' : 'modal-container'
         return (
             <div>
-
                 <div className={containerClass}>
-                    <a onClick={this.toggle} className="close"/>
-                    <div className='employee'>
-                        <div className="col-lg-4 text-left">
-                            <img src={this.props.details.avatar} alt=""/>
-                            <div>{this.props.details.jobTitle}</div>
-                            <div>{this.props.details.age}</div>
-                            <div>{Date(this.props.details.dateJoined)}</div>
-                        </div>
+                    <div className='hidden-xs'>
+                        <a onClick={this.toggle} className="close"/>
+                        this is desktop
+                        <div className='employee'>
+                            <div className="col-lg-4 text-left">
+                                <img src={this.props.details.avatar} alt=""/>
+                                <div>{this.props.details.jobTitle}</div>
+                                <div>{this.props.details.age}</div>
+                                <div>{Date(this.props.details.dateJoined)}</div>
+                            </div>
 
-                        <div className="col-lg-8 detail">
-                            <div>
+                            <div className="col-lg-8 detail">
+                                <div>
                                 <span className='name'> {this.props.details.firstName} &nbsp;
                                     {this.props.details.lastName}</span>
-                                <hr/>
-                                <div>
-                                    {this.props.details.bio.slice(0, 80)}
+                                    <hr/>
+                                    <div>
+                                        {this.props.details.bio.slice(0, 80)}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
 
                 <div className={coverClass} onClick={this.toggle}></div>
